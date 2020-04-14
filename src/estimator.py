26646e18ea038e8e199a1280d4cf73fc84d04f88
Bob Gitonga <rgitonga07@gmail.com>
2020-04-14 17:42:58 +0300

    estimator chalenge one

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,32 @@
+data = {
+    "region": {
+        "name": "Africa",
+        "avgAge": 19.7,
+        "avgDailyIncomeInUSD": 5,
+        "avgDailyIncomePopulation": 0.71
+    },
+    "periodType": "days",
+    "timeToElapse": 58,
+    "reportedCases": 674,
+    "population": 66622705,
+    "totalHospitalBeds": 1380614
+}
+
+
 def estimator(data):
-  return data
+    currentlyInfected = data["reportedCases"] * 10
+    severeImpact = data["reportedCases"] * 50
+    infectionsByRequestedTime = data["reportedCases"] * 512
+    infectionsByRequestedTime = data["reportedCases"] * 512*50
+    outputData = {
+        "data": {},
+        "impact": {"currentlyInfected": currentlyInfected},
+        "severeImpact": {severeImpact},
+        "infectionsByRequestedTime": {"impact": infectionsByRequestedTime}
+
+
+    }
+    return outputData
+
+
+print(estimator(data))
